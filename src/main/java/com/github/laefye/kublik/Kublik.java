b703ad51e73940c958c2f93ac507487dabd4520b
Laefye <rtpurtooe22@mail.ru>
2023-04-20 14:33:51 +0300

    Inital commit

---
+++ src/main/java/com/github/laefye/kublik/Kublik.java
@@ -0,0 +1,17 @@
+package com.github.laefye.kublik;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Kublik extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
